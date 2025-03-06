9416471a3b0e620a3e5b7c7df97d93e6aedb899e
shintopc <50198933+shintopc@users.noreply.github.com>
2025-03-06 09:56:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,14 @@ function toggleButtons() {
   const additionalButtons = document.getElementById("additionalButtons");
   additionalButtons.classList.toggle("hidden");
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed: ', error);
+      });
+  });
+}
