0b7b3e17d274e19d8a7eb282961c7ebe9ceb85a2
shintopc <50198933+shintopc@users.noreply.github.com>
2025-03-13 16:00:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,18 @@
-const CACHE_NAME = 'StreaMix-app-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
-
-// Install the service worker and cache assets
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+    self.skipWaiting();
 });
 
-// Fetch cached assets when offline
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => caches.delete(cache))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(fetch(event.request));
 });
